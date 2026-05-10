edcc83bfc8d99fd52de76bcdb2628c3a8fa90ab0
Sebastian Ehlert <sehlert@microsoft.com>
2026-05-10 23:20:39 +0200

    Typing

--- src/skala/gpu4pyscf/grids.py
+++ src/skala/gpu4pyscf/grids.py
@@ -1,16 +1,19 @@
 # SPDX-License-Identifier: MIT
 
 from logging import getLogger
+from typing import Any
 
 from gpu4pyscf.dft import gen_grid
+from pyscf import gto
 
 LOG = getLogger(__name__)
 
 
 class UnsortableGrids(gen_grid.Grids):  # type: ignore
-    def build(self, mol=None, with_non0tab=False, **kwargs) -> "UnsortableGrids":
+    def build(
+        self, mol: gto.Mole | None = None, with_non0tab: bool = False, **kwargs: Any
+    ) -> "UnsortableGrids":
         sort_grids = kwargs.pop("sort_grids", None)
-        if sort_grids is None:
+        if sort_grids:
             LOG.debug("sorted grids not supported, forcing unsorted grids")
-        super().build(mol, with_non0tab, sort_grids=False, **kwargs)
-        return self
+        return super().build(mol, with_non0tab, sort_grids=False, **kwargs)
