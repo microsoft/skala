419c6cdb7a8c4f9230295ced14da74a792ac8569
jenswehner <jenswehner@gmail.com>
2026-09-04 11:32:11 +0200

    Remove legacy GauXC integration

--- gauxc/UPSTREAM.md
+++
@@ -1,1 +0,0 @@
-The development version of GauXC with added support for Skala was moved to the `skala` branch of the main GauXC repo at https://github.com/wavefunction91/GauXC/tree/skala.
--- gauxc/examples/cpp/gauxc_integration/cmake/skala-dep-versions.cmake
+++
@@ -1,8 +0,0 @@
-set(Skala_GauXC_URL "https://github.com/microsoft/skala/releases/download/v1.1.1/gauxc-skala-r2.tar.gz")
-set(Skala_GauXC_SHA256 "6659d00522b443f2557a960a9ba0217449d2ee25df809634d6becca23eb0f1ff")
-
-set(Skala_CLI11_URL "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.6.1.tar.gz")
-set(Skala_CLI11_SHA256 "377691f3fac2b340f12a2f79f523c780564578ba3d6eaf5238e9f35895d5ba95")
-
-set(Skala_Eigen3_URL "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz")
-set(Skala_Eigen3_SHA256 "8586084f71f9bde545ee7fa6d00288b264a2b7ac3607b974e54d13e7162c1c72")
